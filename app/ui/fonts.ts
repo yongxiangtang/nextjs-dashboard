41562b5b233ec93df4196c0797eaca82c186cb1e
Yongxiang Tang <ytang@guidewire.com>
2024-03-28 20:15:40 -0700

    Frist commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ 
+    subsets: ['latin'],
+    variable: '--font-inter',
+});
+
+export const lusitana = Lusitana({ 
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-lusitana',
+});
